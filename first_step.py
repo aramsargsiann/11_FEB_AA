fdddef682b8ebc2a5453446e95e0adbc5b09d9da
Aram <aramsarsian777@gamil.com>
2024-02-11 18:21:30 +0400

    Arpine jan, I created the first part of the code

--- first_step.py
+++ first_step.py
@@ -1,6 +1,7 @@
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import numpy as np
-X = np.random.rand(10, 20)
+
 class Main:
     def __init__(self, data, algo, config_path=False):
         self.data = data
@@ -8,22 +9,25 @@ class Main:
         self.config_path = config_path
         self.scores = []
 
-    def params(self, ):
-        if self.config_path == False:
-            for k in self.algo.get_params():
-                x = input(f'Enter values of {self.algo}, {k} = ')
-                # if its empty dont add
-                self.scores.append({k:x})
-            return self.scores
+    def params(self):
+        if not self.config_path:
+            for k in self.algo.get_params().keys():
+                x = input(f'Enter value for {k}: ')
+                if x.strip():
+                    self.scores.append({k: x})
+        return self.scores
 
     def using_algo(self):
         for param_dict in self.params():
             for key, value in param_dict.items():
-                return setattr(self.algo, key, value)
+                setattr(self.algo, key, value)
 
+        self.algo.fit(self.data)
+        score = silhouette_score(self.data, self.algo.labels_)
+        print("Silhouette Score:", score)
 
-
-finall = Main(X, KMeans())
-print(finall.params())
+# Example usage:
+X = np.random.rand(10, 20)
+finall = Main(X, KMeans())  # Example: Setting a default value for n_clusters
 finall.using_algo()
 
